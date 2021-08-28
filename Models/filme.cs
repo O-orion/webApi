b997599c1bbad76ecbc46fd7362596a71464df12
Lucas Viana <lucas.lvtorres@gmail.com>
2021-08-28 10:39:15 -0400

    JESUS CRISTO É O SENHOR! - VALIDANDO OS CAMPOS DA CLASSE FILME

--- Models/filme.cs
+++ Models/filme.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace webApi.Models
 {
     public class filme
     {
+
+    [Required(ErrorMessage = "O campo Titulo é obigatório")] //anotando que esse campo é obrigatório
      public string Titulo {get; set;}
+
+    [Required(ErrorMessage = "O campo Diretor é obrigatório")]
      public string Diretor {get; set;}
+
+    [Required (ErrorMessage = "O campo Genero é obrigatório")]
+    [StringLength(30, ErrorMessage ="O gênero não pode passar de 30 caracteres")]
      public string Genero {get; set;} 
-     public int Duacao {get; set;}  
+
+    [Range(1, 600, ErrorMessage ="O campo Duracação é obrigatório")]
+     public int Duracao {get; set;}  
     }
 }
